b0570a3c101d1ed582d952ae9164bf15676402cc
Dmitry K <dimka.rs@gmail.com>
2021-02-17 17:59:40 +0300

    make bme280 work

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ BOARD ?= ebyte_nrf52805
 OPENOCD ?= $(HOME)/repos/openocd
 
 all:
-	west build -b $(BOARD) -- -DBOARD_ROOT=.
+	west build -b $(BOARD) -- -DBOARD_ROOT=$(CURDIR)
 
 clean:
 	rm -rf ./build
@@ -19,6 +19,23 @@ guiconfig:
 init:
 	@echo "source $(HOME)/zephyrproject/zephyr/zephyr-env.sh"
 
+mcuboot:
+	west build -s $(HOME)/zephyrproject/bootloader/mcuboot/boot/zephyr/ -d build/mcuboot -b $(BOARD) -- -DBOARD_ROOT=$(CURDIR)
+
+flash_mcuboot: build/mcuboot/zephyr/zephyr.hex
+	@echo Flashing: $<
+	$(OPENOCD)/src/openocd \
+		-s $(OPENOCD)/tcl \
+		-f interface/stlink.cfg \
+		-f target/nrf52.cfg \
+		-c init \
+		-c "reset init" \
+		-c halt \
+		-c "nrf5 mass_erase" \
+		-c "flash write_image $<" \
+		-c reset \
+		-c exit
+
 flashst: build/zephyr/zephyr.hex
 	@echo Flashing: $<
 	$(OPENOCD)/src/openocd \
@@ -33,3 +50,11 @@ flashst: build/zephyr/zephyr.hex
 		-c reset \
 		-c exit
 
+reset:
+	$(OPENOCD)/src/openocd \
+		-s $(OPENOCD)/tcl \
+		-f interface/stlink.cfg \
+		-f target/nrf52.cfg \
+		-c init \
+		-c reset \
+		-c exit
--- src/main.c
+++ src/main.c
@@ -8,32 +8,68 @@
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
+#include <drivers/sensor.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
-#define PIN (4)
+#define SLEEP_TIME_MS (1000)
+#define LED_PIN (4)
+#define SCL_PIN (5)
+#define SDA_PIN (12)
+
+#define BME280 DT_INST(0, bosch_bme280)
+#if DT_NODE_HAS_STATUS(BME280, okay)
+#define BME280_LABEL DT_LABEL(BME280)
+#else
+#error Your devicetree has no enabled nodes with compatible "bosch,bme280"
+#define BME280_LABEL "<none>"
+#endif
+
 
 void main(void)
 {
-	const struct device *dev;
-	bool led_is_on = true;
-	int ret;
+    const struct device *gpio0;
+    const struct device *bme280;
+    bool led_is_on = true;
+    int ret;
 
-	printk("Build date: " __DATE__ " " __TIME__"\n");
+    printk("Build date: " __DATE__ " " __TIME__"\n");
 
-	dev = device_get_binding("GPIO_0");
-	if (dev == NULL) {
-		return;
-	}
+    gpio0 = device_get_binding("GPIO_0");
+    if (gpio0 == NULL)
+    {
+        return;
+    } else {
+        ret = gpio_pin_configure(gpio0, LED_PIN, GPIO_OUTPUT);
+        if (ret < 0)
+        {
+            return;
+        }
+    }
 
-	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT);
-	if (ret < 0) {
-		return;
-	}
+    bme280 = device_get_binding(BME280_LABEL);
+    if (bme280 == NULL) {
+        printk("%s not found\n", BME280_LABEL);
+    } else {
+        printk("%s found\n", BME280_LABEL);
+    }
 
-	while (1) {
-		gpio_pin_set(dev, PIN, (int)led_is_on);
-		led_is_on = !led_is_on;
-		k_msleep(SLEEP_TIME_MS);
-	}
+    while (1) {
+        struct sensor_value temp, press, humidity;
+
+        if (bme280 != NULL)
+        {
+            sensor_sample_fetch(bme280);
+            sensor_channel_get(bme280, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+            sensor_channel_get(bme280, SENSOR_CHAN_PRESS, &press);
+            sensor_channel_get(bme280, SENSOR_CHAN_HUMIDITY, &humidity);
+
+            printk("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
+                temp.val1, temp.val2, press.val1, press.val2,
+                humidity.val1, humidity.val2);
+        }
+
+        gpio_pin_set(gpio0, LED_PIN, (int)led_is_on);
+        led_is_on = !led_is_on;
+        k_sleep(K_MSEC(SLEEP_TIME_MS));
+    }
 }
